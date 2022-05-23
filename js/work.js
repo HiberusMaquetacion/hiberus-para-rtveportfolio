0ae3674e25cfe71a4fc4ec4395bc0df9eb073c7c
beatriz-jc <92922446+beatriz-jc@users.noreply.github.com>
2022-05-23 10:59:52 +0200

    Update work.js

--- js/work.js
+++ js/work.js
@@ -10,69 +10,9 @@ $(function(){
     $('.filter-work div ul li a[href="#'+id+'"]').addClass('active');
     
     $('ul.filter-sections li a').click(function() {
-    var textoFiltro = $(this).text().toLowerCase();
-    if(textoFiltro == 'noticias'){   
-        document.getElementById("noticias").style.display = "";
-        document.getElementById("deportes").style.display = "none";
-        document.getElementById("television").style.display = "none";
-        document.getElementById("radio").style.display = "none";
-        document.getElementById("otros").style.display = "none";
-    } else if (textoFiltro == 'deportes') {
-        console.log('estoy en deportes');
-
-        document.getElementById("noticias").style.display = "none";
-        document.getElementById("deportes").style.display = "";
-        document.getElementById("television").style.display = "none";
-        document.getElementById("radio").style.display = "none";
-        document.getElementById("otros").style.display = "none";
-
-        document.getElementById("titulardeportes").style.display = "none";
-        
-    } else if (textoFiltro == 'radio') {
-        console.log('estoy en radio');
-        document.getElementById("noticias").style.display = "none";
-        document.getElementById("deportes").style.display = "none";
-        document.getElementById("television").style.display = "none";
-        document.getElementById("radio").style.display = "";
-        document.getElementById("otros").style.display = "none";
-   
-        document.getElementById("titularradio").style.display = "none";
-
-    } else if (textoFiltro == 'otros') {
-        console.log('estoy en otros');
-        document.getElementById("noticias").style.display = "none";
-        document.getElementById("deportes").style.display = "none";
-        document.getElementById("television").style.display = "none";
-        document.getElementById("radio").style.display = "none";
-        document.getElementById("otros").style.display = "";
-
-        document.getElementById("titularotros").style.display = "none";
-
-    } else if (textoFiltro == 'televisión') {
-        console.log('estoy en television');
-
-        document.getElementById("noticias").style.display = "none";
-        document.getElementById("deportes").style.display = "none";
-        document.getElementById("television").style.display = "";
-        document.getElementById("radio").style.display = "none";
-        document.getElementById("otros").style.display = "none";
-
-        document.getElementById("titulartele").style.display = "none";
-
-
-    } else {
-        document.getElementById("noticias").style.display = "";
-        document.getElementById("deportes").style.display = "";
-        document.getElementById("television").style.display = "";
-        document.getElementById("radio").style.display = "";
-        document.getElementById("otros").style.display = "";
-
-        document.getElementById("titulartele").style.display = "";
-        document.getElementById("titularotros").style.display = "";
-        document.getElementById("titularradio").style.display = "";
-        document.getElementById("titulardeportes").style.display = "";
- 
-    }
-    return false;
+        var textoFiltro = $(this).text().toLowerCase();
+        $('section.gallery[id!="'+id+'"]').addClass('hidden');
+        $('.filter-work div ul li a[href="#'+textoFiltro+'"]').addClass('active');
+    
     });
 });
